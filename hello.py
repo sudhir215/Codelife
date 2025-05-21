97269c1023747cc2bd0f3672f7feb2b6941e008e
SUDHIR <sudhir0436@gmail.com>
2025-05-21 14:20:20 +0530

    Add files via upload

---
+++ hello.py
@@ -0,0 +1,9 @@
+x=int(input("Enter a number: "))
+if x % 2 == 0:
+    print("Even")
+else:
+    print("Odd")
+
+print("This will be added in the local envoirement")
+print("changes in  the local envoirement done")
+print("making changes in the new branch")
